4d24d2b29230d032d7c1f23aa0bf6d358f71e0c0
tulolo287 <iv1055@yandex.ru>
2023-03-26 14:13:35 +0300

    ff

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const os = require("os")
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
+console.log(__dirname);
+console.log(__filename);
+console.log(global);
--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -1,85 +1,109 @@
+import { Form, Input, Button } from "antd";
 import { useRef, useState, useEffect } from "react";
 
-const EMAIL_REGEX = /[a-zA-Z0-9]{3,20}/g;
+const EMAIL_REGEX = /^[a-zA-Z][a-zA-Z0-9]{3,20}$/;
 const PWD_REGEX = /^[a-zA-Z0-9-_]{6,20}$/;
 
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+
 const Register = () => {
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passRef = useRef<HTMLInputElement>(null);
-  const btnRef = useRef(null);
+  const [form] = Form.useForm();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPass, setConfirmPass] = useState("");
-  const [errorEmail, setErrorEmail] = useState(true);
-  const [errorPassword, setErrorPassword] = useState(true);
-  const [errorConfirmPass, setErrorConfirmPass] = useState(true);
-  const [isDisabled, setDisabled] = useState(true);
-
-  useEffect(() => {
-    if (!errorEmail && !errorPassword && !errorConfirmPass) {
-      setDisabled(false);
-    } else {
-      setDisabled(true);
-    }
-  }, [errorEmail, errorPassword, errorConfirmPass]);
-
-  const handleEmail = (e: any) => {
-    if (e.currentTarget.value.match(EMAIL_REGEX)) {
-      setEmail(e.currentTarget.value);
-      setErrorEmail(false);
-    } else {
-      setErrorEmail(true);
-    }
-  };
-  const handlePassword = (e: any) => {
-    if (e.currentTarget.value.length > 5) {
-      setPassword(e.currentTarget.value);
-      setErrorPassword(false);
-    } else {
-      setErrorPassword(true);
-    }
-  };
-  const handleConfirm = (e: any) => {
-    if (e.currentTarget.value.length > 5) {
-      setConfirmPass(e.currentTarget.value);
-      setErrorConfirmPass(false);
-    } else {
-      setErrorConfirmPass(true);
-    }
+  const onFinish = (values: any) => {
+    console.log('Received values of form: ', values);
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    //console.log(emailRef.current?.value);
-    //console.log(passRef.current?.value);
-    const formData = new FormData(e.currentTarget);
-    const [email, password] = [
-      formData.get("email")?.toString(),
-      formData.get("password")?.toString(),
-    ];
-    //const formProps = Object.fromEntries(formData);
-    console.log(email, password);
-  };
+ 
   return (
     <>
       Register
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input onChange={handleEmail} ref={emailRef} name="email" />
-        <label htmlFor="password">Password</label>
-        <input
-          onChange={handlePassword}
-          ref={passRef}
-          name="password"
-          type="password"
-        />
-        <label htmlFor="password">Confirm password</label>
-        <input onChange={handleConfirm} name="confirmPass" type="password" />
-        <button ref={btnRef} disabled={isDisabled} type="submit">
+      <Form 
+     {...formItemLayout}
+      form={form}
+      name="register"
+      onFinish={onFinish}
+      style={{ maxWidth: 600 }}
+      scrollToFirstError>
+      <Form.Item
+        name="email"
+        label="E-mail"
+        rules={[
+          {
+            type: 'email',
+            message: 'The input is not valid E-mail!',
+          },
+          {
+            required: true,
+            message: 'Please input your E-mail!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+        hasFeedback
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="confirm"
+        label="Confirm Password"
+        dependencies={['password']}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: 'Please confirm your password!',
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject(new Error('The two passwords that you entered do not match!'));
+            },
+          }),
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
           Register
-        </button>
-      </form>
+        </Button>
+      </Form.Item>
+      </Form>
     </>
   );
 };
